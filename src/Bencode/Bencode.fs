e1a4cd1a9ee37c6e482bef9c35a5c747423443be
polkduran <polkduran@gmail.com>
2014-07-22 00:08:19 +0200

    BenString + (bug to track)

--- src/Bencode/Bencode.fs
+++ src/Bencode/Bencode.fs
@@ -13,34 +13,34 @@ let (|Num|_|) (c:char) = match Int32.TryParse(c.ToString()) with
                             | true,_ -> Some(c)
                             | _ -> None
 
-let toInt = List.rev >> toString >> int64
+let toInt = List.rev >> toString >> (fun s -> printfn "bla %s" s; s) >> int64
 
-let parseInt prefix chars = 
-    let (|Nums|_|) suffix chars =
+let parseInt (suffix:char) chars = 
+    let (|Nums|_|) chars =
         let rec parseInternal acc = function
             |Num(c)::tail -> parseInternal (c::acc) tail
             |suffix::tail -> Some(toInt acc, tail)
             |_ -> None
         parseInternal [] chars
     match chars with
-    | Nums prefix (i, tail) -> Some(i,tail)
+    | Nums (i, tail) -> Some(i,tail)
     | _ ->None
 
 let (|BenInt|_|) chars = 
     chars 
         |> function 'i'::r -> Some(r) |_->None
         |> Option.bind (parseInt 'e')
-        |> Option.map (fun (i,tail) -> Some(Int(i), tail)
+        |> Option.map (fun (i,tail) -> Int(i), tail)
         
 let (|BenString|_|) chars =
     let toBenString = List.rev >> toString >> String
-    let rec parseString acc n = function
-        | s::tail when n > 0L -> parseString  (s::acc) (n-1L) tail
-        | _ when n > 0L -> None
-        | rest -> Some(toBenString acc, rest)
-    parseInt ':' chars
-//        |> function n::':'::r -> Some(n.ToString() |> int,r) |_->None
-//        |> Option.bind (fun (n,r) -> parseString [] n r)
+    let rec parseString acc = function
+        | n,s::tail when n > 0L -> parseString  (s::acc) (n-1L, tail)
+        | n,_ when n > 0L -> None
+        | _,rest -> Some(toBenString acc, rest)
+    chars 
+        |> parseInt ':'
+        |> Option.bind (parseString [])
 
 let rec (|BenList|_|) chars =
     let toBenList = List.rev >> List
@@ -64,9 +64,6 @@ and (|BenDic|_|) chars =
     let rec parseKv acc = function
         | BenString(String(k), BenEl(v,tail)) 
                 -> parseKv ((k,v)::acc) tail
-//        | BenString(String(k), r) ->
-//            match r with | BenEl(x,tail) -> parseKv ((k,x)::acc) tail
-//                         |_ -> None 
         | 'e'::tail -> Some(buildMap acc, tail)
         | _ -> None
     chars
@@ -79,7 +76,7 @@ let f chars =
     |c -> (None,c)
 
 let mutable str = "l9:publisher3:bob17:publisher-webpage15:www.example.com18:publisher.location4:homee"
-str<-"17:publisher-webpage"
+//str<-"l17:publisher-webpagee"
 //str<-"d9:publisherle4:spami456ee"
 let chars = str |> List.ofSeq
 
