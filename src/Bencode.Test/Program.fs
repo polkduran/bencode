c1ffa8cbf50e19031e50115f9f59a79172d4b0f7
polkduran <polkduran@gmail.com>
2014-07-24 23:43:04 +0200

    Handling negative numbers, Refactoring

--- src/Bencode.Test/Program.fs
+++ src/Bencode.Test/Program.fs
@@ -6,6 +6,14 @@ let main argv =
         |Bencode.BenEl(x,tail) -> (Some(x),tail)
         |c -> (None,c)
 
+//    let nums = ["0e";"1e";"32e";"-256e";"-012e";"-0e";"45ie"]
+//    let r = nums 
+//            |> List.map List.ofSeq
+//            |> List.map (Bencode.tryGetNums 'e')
+//
+//    
+//    printfn "%A" r
+
     let mutable str = "d9:publisher3:bob17:publisher-webpage15:www.example.com18:publisher.location4:homee"
     //str<-"17:publisher-webpage"
     //str<-"d9:publisherle4:spami456ee"
--- src/Bencode/Bencode.fs
+++ src/Bencode/Bencode.fs
@@ -7,44 +7,60 @@ type BenElement =
     | List of BenElement list
     | Dict of Map<string,BenElement>    
 
-let toString chars = System.String(chars |> Array.ofSeq)
+let private toStringRev chars = System.String(chars |> List.rev |> Array.ofSeq)
+let private toInt = toStringRev >> int64
 
-let (|Num|_|) (c:char) = 
-    let intVal = int c
-    if intVal > 47 && intVal < 58 then Some(c) else None
+let private prefixed prefix = function 
+                | head::tail when head = prefix -> Some(tail)
+                | _ -> None
 
-let toInt = List.rev >> toString >> int64
+let private (|Num|_|) (c:char) = if c >= '0' && c <= '9' then Some(c) else None
+let private (|End|_|) = function 'e'::tail -> Some(tail) |_ -> None
 
-let (|Nums|_|) suffix chars = 
+let private tryGetNums suffix chars =
     let rec parseInternal acc = function
             |Num(c)::s::tail when s = suffix -> Some(toInt (c::acc), tail)
             |Num(c)::tail -> parseInternal (c::acc) tail
             |_ -> None
-    parseInternal [] chars 
+    match chars with
+        |'-'::'0'::tail -> None
+        |'-'::tail -> parseInternal ['-'] tail
+        |_ as tail -> parseInternal [] tail
 
-let (|BenInt|_|) = function
-    |'i'::Nums 'e' (i, tail) -> Some(Int(i),tail)
-    |_ -> None
+let (|BenInt|_|) chars =
+    chars 
+        |> prefixed 'i'
+        |> Option.bind (tryGetNums 'e')
+        |> Option.map (fun (i,tail) -> Int i,tail)
         
 let (|BenString|_|) chars =
-    let toBenString = List.rev >> toString >> String
+    let toBenString = toStringRev >> String
     let rec parseString acc = function
         | n,s::tail when n > 0L -> parseString  (s::acc) (n-1L, tail)
         | n,_ when n > 0L -> None
         | _,rest -> Some(toBenString acc, rest)
     chars 
-        |> function Nums ':' (i,tail) -> Some(i,tail)|_->None
+        |> tryGetNums ':'
         |> Option.bind (parseString [])
  
 let rec (|BenList|_|) chars =
-    let toBenList = List.rev >> List
     let rec parseList acc = function
         |BenEl(x,tail) -> parseList (x::acc) tail
-        |'e'::tail -> Some(toBenList acc, tail)
+        |End(tail) -> Some(acc |> List.rev |> List, tail)
         |_ -> None
     chars
-        |> function 'l'::r -> Some(r) |_-> None
+        |> prefixed 'l'
         |> Option.bind (parseList [])
+
+and (|BenDic|_|) chars =
+    let buildMap kvs = Dict( new Map<string,BenElement>(kvs) )
+    let rec parseKv acc = function
+        | BenString(String(k), BenEl(v,tail)) -> parseKv ((k,v)::acc) tail
+        | End(tail) -> Some(buildMap acc, tail)
+        | _ -> None
+    chars
+        |> prefixed 'd'
+        |> Option.bind (parseKv [])
 
 and (|BenEl|_|) = function
     | BenInt(x,tail) 
@@ -52,15 +68,5 @@ and (|BenEl|_|) = function
     | BenDic(x, tail)
     | BenString(x,tail) -> Some(x,tail)
     | _ -> None
-
-and (|BenDic|_|) chars =
-    let buildMap kvs = Dict( new Map<string,BenElement>(kvs) )
-    let rec parseKv acc = function
-        | BenString(String(k), BenEl(v,tail)) -> parseKv ((k,v)::acc) tail
-        | 'e'::tail -> Some(buildMap acc, tail)
-        | _ -> None
-    chars
-        |> function 'd'::r -> Some(r) |_-> None
-        |> Option.bind (parseKv [])
 
 
