65330d8ab4b938e2d469e8b1136ba768dad31924
Pablo Fernandez <pfernandez@talentsoft.com>
2014-07-29 13:51:28 +0200

    Add assembly information

---
+++ src/Bencode/AssemblyInfo.fs
@@ -0,0 +1,11 @@
+﻿namespace Bencode
+    open System.Reflection
+    
+    [<assembly:AssemblyDescription("F# Bencode parser.")>]
+    [<assembly: AssemblyCopyright("Copyright Pablo Fernandez Duran 2014")>]
+    [<assembly:AssemblyTitle("Bencode")>]
+    [<assembly:AssemblyProduct("Bencode")>]
+    [<assembly:AssemblyVersion("0.1.0.0")>]
+    [<assembly:AssemblyFileVersion("0.1.0.0")>]
+
+    do()
--- src/Bencode/Bencode.fs
+++ src/Bencode/Bencode.fs
@@ -3,12 +3,14 @@ open BencodeParser
 
 module BenDecode =
 
+    /// Parses a list of chars as a Bencode element
     let parseChars chars = 
         match chars with
         | BenEl(el, []) -> el
         | BenEl(el, tail) -> sprintf "Bad format at the ending of the bencode string %A" tail |> failwith
         | _ -> failwith "Cannot parse the bencode string"
 
+    /// Parses a string as a Bencode element
     let parseString (bencodeString:string) = 
             bencodeString |> List.ofSeq |> parseChars
     
